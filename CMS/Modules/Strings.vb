be8d16b41e5c883ba971a7ef3e6dc1c02e77289c
Bobby Tzenev <mercur@gmail.com>
2014-08-22 12:13:34 -0400

    Added String compare function

--- CMS/Modules/Strings.vb
+++ CMS/Modules/Strings.vb
@@ -127,6 +127,43 @@ Public Module _Strings
 
 #End Region
 
+#Region " Levenshtein "
+    Public Function Distance(ByVal s As String, ByVal t As String) As Integer
+        Dim n As Integer = s.Length
+        Dim m As Integer = t.Length
+        Dim d(n + 1, m + 1) As Integer
+        If n = 0 Then
+            Return m
+        End If
+        If m = 0 Then
+            Return n
+        End If
+        Dim i As Integer
+        Dim j As Integer
+        For i = 0 To n
+            d(i, 0) = i
+        Next
+        For j = 0 To m
+            d(0, j) = j
+        Next
+        For i = 1 To n
+            For j = 1 To m
+                Dim cost As Integer
+                If t(j - 1) = s(i - 1) Then
+                    cost = 0
+                Else
+                    cost = 1
+                End If
+
+                d(i, j) = Math.Min(Math.Min(d(i - 1, j) + 1, d(i, j - 1) + 1),
+                           d(i - 1, j - 1) + cost)
+            Next
+        Next
+        Return d(n, m)
+    End Function
+
+#End Region
+
 #Region " StrRepeat "
 
   Public Function StrRepeat(ByVal vstr_value As String, ByVal vint_count As Integer) As String
